bf326311274f554ea53904e2100a18574b164396
zetawave <fabrizioalor@yahoo.it>
2020-12-11 13:45:11 +0100

    First real commit

---
+++ src/unicompare.js
@@ -0,0 +1,9 @@
+/**
+ * Compare two objects
+ * @param {Object} obj1 -> First object
+ * @param {Object} obj2 -> Second object
+ * @param {Boolean} deep -> Deep compare
+ */
+exports.objEqual = function(obj1, obj2, deep){
+    
+}
